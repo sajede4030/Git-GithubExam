ea348c99d5c1beeb3ec684aff341cf623cc3801b
HamedFahimi <96087367+HamedFahimi@users.noreply.github.com>
2025-06-03 07:09:54 +0330

    Initial upload of files

---
+++ DisplaySimpleFigureWithJava-InitialVersion.java
@@ -0,0 +1,16 @@
+public class DisplaySimpleFigureWithJava {
+    public static void main(String[] args) { 
+        ninety();  
+        System.out.println();
+        ninety();
+        ninety();  
+    }
+
+    public static void ninety() {
+        System.out.println("* | | | | | *");
+    }
+}
+
+
+
+
