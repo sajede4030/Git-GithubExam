0f0ee180bb60119a291fb41e91ebf9b8e88e88c6
sajede <sajdhfrzanh@gmail.com>
2025-06-03 10:54:55 -0800

    change file

---
+++ DisplaySimpleFigureWithJava.java
@@ -0,0 +1,22 @@
+public class DisplaySimpleFigureWithJava {
+    public static void main(String[] args) { 
+        ninety();  
+        System.out.println();
+        ninety();
+        ninety();  
+        stars(); 
+    }
+
+    public static void ninety() {
+        System.out.println("* | | | | | *");
+    }
+
+    public static void stars() {
+        System.out.println("       *****      ");
+        System.out.println("     *********     ");
+        System.out.println("   *************    ");
+    }
+}
+
+
+
